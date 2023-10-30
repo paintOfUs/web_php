4b64cffbd95b217f7b7ff78325aa2269635b0f0d
paintOfUs <dominhtuan_t65@gmail.com>
2023-10-30 22:12:06 +0700

    frame website comic

---
+++ body.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+img {
+	height: 300px;
+}
+</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col cols-4">
+				<div class="card h-100">
+					<img src="/WebComic/view/asset/img/1.jpg" class="card-img-top"
+						alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text
+							below as a natural lead-in to additional content. This content is
+							a little bit longer.</p>
+						<div class="d-flex justify-content-between">
+							<button class="btn btn-primary me-md-2" type="button">Button</button>
+							<button class="btn btn-danger text-end" type="button">Button</button>
+						</div>
+					</div>
+					<div class="card-footer">
+						<small class="text-body-secondary">Last updated 3 mins ago</small>
+					</div>
+				</div>
+			</div>
+			<div class="col cols-4">
+				<div class="card h-100">
+					<img src="/WebComic/view/asset/img/1.jpg" class="card-img-top"
+						alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text
+							below as a natural lead-in to additional content. This content is
+							a little bit longer.</p>
+						<div class="d-flex justify-content-between">
+							<button class="btn btn-primary me-md-2" type="button">Button</button>
+							<button class="btn btn-danger justify-item-end" type="button">Button</button>
+						</div>
+					</div>
+					<div class="card-footer">
+						<small class="text-body-secondary">Last updated 3 mins ago</small>
+					</div>
+				</div>
+			</div>
+			<div class="col cols-4">
+				<div class="card h-100">
+					<img src="/WebComic/view/asset/img/1.jpg" class="card-img-top"
+						alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text
+							below as a natural lead-in to additional content. This content is
+							a little bit longer.</p>
+						<div class="d-flex justify-content-between">
+							<button class="btn btn-primary me-md-2" type="button">Button</button>
+							<button class="btn btn-danger justify-item-end" type="button">Button</button>
+						</div>
+
+					</div>
+					<div class="card-footer">
+						<small class="text-body-secondary">Last updated 3 mins ago</small>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<br>
+	<br>
+	<nav aria-label="Page navigation example">
+		<ul class="pagination justify-content-center">
+			<li class="page-item disabled"><a class="page-link">Previous</a></li>
+			<li class="page-item"><a class="page-link" href="#">1</a></li>
+			<li class="page-item"><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item"><a class="page-link" href="#">Next</a></li>
+		</ul>
+	</nav>
+	<br>
+	<br>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<nav id="menu" class="navbar navbar-expand-lg  stick-top" style="background-color:#84CDCA;">
+		<div class="container-fluid">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white">
+					<li class="nav-item"><a class="nav-link active" aria-current="page"
+						href="index.php">Trang chủ</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Tủ sách</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Hỗ trợ</a></li>
+				</ul>
+				<form class="d-flex me-2" role="search">
+					<input class="form-control me-2" type="search" placeholder="Search"
+						aria-label="Search">
+					<button class="btn btn-light btn-outline-danger" stype="submit">
+						<i class="fa-solid fa-magnifying-glass"></i>
+					</button>
+				</form>
+
+				<div class="d-flex  text-end">
+					<a href="loginForm.php"><button type="button" class="btn btn-primary px-3 me-2">Login</button></a>
+					<a href="regist.php"><button type="button" class="btn btn-danger me-2">Sign up</button></a>
+				</div>
+			</div>
+		</div>
+		</div>
+	</nav>
+	<br>
+</body>
+</html>
